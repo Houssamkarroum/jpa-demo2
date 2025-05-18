af0cecbdb0778b173c20190709d39345bc197245
legend <karroumhoussam2020@gmail.com>
2025-05-18 22:12:44 +0100

    add some comments and improve the logique behind the User controller file

--- src/main/java/ma/houssam/jpademo/web/UserController.java
+++ src/main/java/ma/houssam/jpademo/web/UserController.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// Marks this class as a REST controller, allowing it to handle HTTP requests
 @RestController
 public class UserController {
+
+    // Injects the UserService dependency
     @Autowired
     UserService userService;
+
+    // Handles GET requests to /users/{username}
+    // @param username the username to search for
+    // @return the User object matching the given username
     @GetMapping("/users/{username}")
     public User user(@PathVariable String username) {
         return userService.findUserByUserName(username);
